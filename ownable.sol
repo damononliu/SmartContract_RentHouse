80a33b9502836109123d383a4e7d0eac8b8310fd
Damon <damon@example.com>
2018-04-19 16:55:00 +0800

    引用zeppelin-solidity库中的ownable.sol，检查合约权限

---
+++ ownable.sol
@@ -0,0 +1,42 @@
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    //判断newOwner的地址不为0；
+    require(newOwner != address(0));
+    //触发OwnershipTransferred事件，用于更新前端，并且用indexed修饰，只
+    //提醒特定前端用户
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
